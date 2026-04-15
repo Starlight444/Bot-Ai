5b41426fb811c0a52d23470b487ac0e2a2c6fbba
chalisa <chalisa7349@gmail.com>
2026-04-16 01:11:50 +0700

    change port

--- app.py
+++ app.py
@@ -116,6 +116,5 @@ def handle_line_message(event):
  
  
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
